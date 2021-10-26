5ba4cd524fc41cf2a520ebde62d852f1151afab8
Brenet <jrm.devweb@gmail.com>
2021-10-26 09:10:32 +0200

    Fin Session DB

--- peps/core/SessionDB.php
+++ peps/core/SessionDB.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 
 namespace peps\core;
 
+use PDO;
+use PDOException;
 use SessionHandlerInterface;
 
 
@@ -18,6 +20,17 @@ use SessionHandlerInterface;
  */
 class SessionDB implements SessionHandlerInterface
 {
+    /**
+     * Vrai si session périmée, faux sinon
+     */
+    protected static bool $expired = false;
+
+    /**
+     * Undocumented variableDurée maxi de la session (secondes).
+     *
+     * @var integer
+     */
+    protected static int $timeout;
 
 
     /**
@@ -29,6 +42,9 @@ class SessionDB implements SessionHandlerInterface
      */
     public static function init(int $timeout, string $mode, string $sameSite): void
     {
+        //* Définir le timeout.
+        self::$timeout = $timeout;
+
         //* Définir la durée de vie du cookie.
         match ($mode) {
             Cfg::get('SESSION_PERSISTENT') => ini_set('session.cookie_lifetime', (string)(86400 * 365 * 20)), //* 20ans (expiration gérée coté serveur).
@@ -48,53 +64,127 @@ class SessionDB implements SessionHandlerInterface
         ini_set('session.trans_sid', '0');
         //* Mitiger les attaques XSS (Cross Site scripting = injections) en interdisant l'accès aux cookies via JS.
         ini_set('session.cookie_httponly', '1');
-
-
-
-
-
+        //* Mitiger les attaques SFA (Session Site Attack) en refusant les cookies non générés par PHP.
+        ini_set('session.use_strict_mode', '1');
+        //* Mitiger les attaques CSRF (Cross Site Request Forgery)
+        ini_set('session.cookie_samesite', '1');
+        //* Définir une instance de cette classe comme gestionnaire des sessions.
         session_set_save_handler(new self());
+        //* Démarrer la session.
         session_start();
+        //* Si session expirée, la détruire et en démarrer une nouvelle.
+        if (self::$expired) {
+            session_destroy();
+            self::$expired = false;
+            session_start();
+        }
     }
-
+    /**
+     * Inutile ici.
+     *
+     * @param string $path Chemin du fichier de sauvegarde de la session.
+     * @param string $name Nom de la session. (PHPSESSID par défaut)
+     * @return boolean Pour un usage interne PHP, ici systématiquement true.
+     */
     public function open($path, $name): bool
     {
-        var_dump('Open');
         return true;
     }
-
+    /**
+     * Lire et retourner les données de session.
+     *
+     * @param string $id SID. 
+     * @return string|false Données de session sérialisées (PHP) ou false si lecture impossible.
+     */
     public function read($id): string|false
     {
-        var_dump("read {$id}");
+        //* Créer la requête.
+        $q = "SELECT * FROM session WHERE sid = :sid";
+        $params = [':sid' => $id];
+        //*Exécuter la requête et si une session est retrouvée, vérifier sa validité.
+        if ($objSession = DBAL::get()->xeq($q, $params)->findOne()) {
+            //* Si expirée, passer le booléen $expired à true et retourner une chaîne vide.
+            if (strtotime($objSession->dateSession) + self::$timeout < time()); {
+                self::$expired = true;
+                return '';
+            }
+            //* Sinon, retourner les données.
+            return $objSession->data;
+        }
+        //* Si pas encore de session, retourner une chaîne vide.
         return '';
     }
 
+    /**
+     * Ercit les données de session.
+     *
+     * @param string $id SID
+     * @param string $data Données de session
+     * @return boolean Pour usage interne PHP, ici systématiquement true.
+     */
     public function write($id, $data): bool
     {
+        if (!self::$expired) {
 
-        var_dump("write {$id}: {$data}");
+            //* Tenter une requête INSERT.
+            try {
+                $q = 'INSERT INTO session VALUES(:sid, :data, :dateSession)';
+                $params = [':sid' => $id, ':data' => $data, ':dateSession' => date('Y-m-d H:i:s')];
+                DBAL::get()->xeq($q, $params);
+            }
+            //* Si erreur (doublon de SID), exécuter une requête UPDATE.
+            catch (PDOException $e) {
+                $q = 'UPDATE session SET data =  :data, :dateSession = :dateSession WHERE sid = :sid';
+                $params = [':sid' => $id, ':data' => $data, ':dateSession' => date('Y-m-d H:i:s')];
+                DBAL::get()->xeq($q, $params);
+            }
+        }
         return true;
     }
 
-
+    /**
+     * Inutile ici.
+     *
+     * @return boolean Pour un usage interne PHP, ici systématiquement true.
+     */
     public function close(): bool
     {
-        var_dump('Close');
         return true;
     }
 
-
+    /**
+     * Détruire la session (cookie + DB).
+     *
+     * @param string $id SID.
+     * @return boolean Pour un usage interne PHP, ici systématiquement true.
+     */
     public function destroy($id): bool
     {
-        var_dump("destroy {$id}");
+        //* Récupérer le nom de la session.
+        $sessionName = session_name();
+        //* Supprimer le cookie du navigateur.
+        setcookie($sessionName, '', 1, '/',);
+        //* Supprimer la clé du tableau des cookies du serveur.
+        unset($_COOKIE[$sessionName]);
+        //* Supprimer la session de la DB.
+        $q = "DELETE FROM session WHERE sid = :sid";
+        $params = [':sid' => $id];
+        DBAL::get()->xeq($q, $params);
+        //* Retourner systématiquement true        
         return true;
     }
 
-
+    /**
+     * Garbage collector, supprime les sessions expirées en DB.
+     * @param int $maxlifetime Durée de vie maxi d'une session (secondes).
+     * @return integer|false True si la suppression a réussi, false sinon.
+     */
     public function gc($maxlifetime): int|false
     {
-        var_dump('GarbageCol {$maxlifetime}');
-
-        return 3;
+        //* Créer la requête.
+        $q = "DELETE FROM session WHERE dateSession < :dateMin";
+        $params = [':dateMin' => date('Y-m-d H:i:s', time() - $maxlifetime)];
+        //* Retourner le nombre d'enregistrements.
+        return DBAL::get()->xeq($q, $params)->nb();
     }
 }
